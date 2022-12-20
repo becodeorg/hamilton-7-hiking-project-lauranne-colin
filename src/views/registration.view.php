11ec63dca7b4cfe1925a7cba5d0aada0f563c037
Colin <colin25.huart@hotmail.com>
2022-12-20 16:08:06 +0100

    merged -- temp

--- src/views/registration.view.php
+++ src/views/registration.view.php
@@ -11,12 +11,7 @@
         border-bottom-right-radius: 0.75rem;
     }
 
-<<<<<<< HEAD
-<form action="registration" method="post">
-    <div>
-        <label for="nickname">Username</label>
-        <input type="text" name="username">
-=======
+
 </style>
 
 <h2 class="ml-5 my-5">Create your account</h2>
@@ -30,7 +25,6 @@
     <div class="input flex flex-row mb-5 w-fit">
         <label for="email" class="w-28 label uppercase bg-lime-300 p-2 text-xs">Email</label>
         <input type="email" name="email" placeholder="..." class="input border-stone-200 border-2 w-36 pl-2 text-xs">
->>>>>>> da19b34ed09f41abfb283687a9d3f4fb982633a5
     </div>
     <div class="input flex flex-row mb-5 w-fit">
         <label for="password" class="w-28 label uppercase bg-lime-300 p-2 text-xs">Password</label>
