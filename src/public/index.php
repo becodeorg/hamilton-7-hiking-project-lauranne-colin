577992c9795e39be821e15d5426f2be8ce812d4d
l4ur4nn3 <nicolaslauranne99@gmail.com>
2022-12-20 10:48:59 +0100

    moving some data into other files

--- src/public/index.php
+++ src/public/index.php
@@ -58,9 +58,14 @@ if ($url === 'registration') {
 
 
 <head>
-    <meta charset="UTF-8">
+    <!-- <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title -->
     <link href="./index.css" rel="stylesheet">
+    <!-- <style>
+        body {
+            background-color: rgb(217 119 6);
+        }
+    </style> -->
 </head>
--- src/views/includes/header.view.php
+++ src/views/includes/header.view.php
@@ -2,32 +2,37 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="icon" type="image" href="./icon/01d.png" />
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://kit.fontawesome.com/d74732e987.js" crossorigin="anonymous"></script>
     <style> 
         @import url('https://fonts.googleapis.com/css2?family=Mystery+Quest&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
         h1 {
             font-family: 'Mystery Quest', cursive;
         }
+        nav {
+            font-family: 'Montserrat', sans-serif;
+        }
     </style>
-    <title>Hiking Project</title>
+    <title>Running Up That Hill</title>
 </head>
 <body>
 <header>
-    <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
+    <h1 class="text-5xl flex pt-12 pb-6 justify-center text-center">RUNNING UP</br>THAT HILL</h1>
+    <nav class="pb-3">
+        <ul class="text-sm flex flex-row justify-center">
+            <li class="mx-2"><a href="/"><i class="fa-solid fa-house"></i><span class="hidden">Home</span></a></li>
             <?php if (empty($_SESSION['user'])): ?>
-                <li><a href="/login">Login</a></li>
-                <li><a href="/registration">Register</a></li>
+                <li class="mx-2"><a href="/login"><i class="fa-solid fa-user-check"></i><span class="hidden">Login</span></a></li>
+                <li class="mx-2"><a href="/registration"><i class="fa-solid fa-user-plus"></i><span class="hidden">Register</span></a></li>
             <?php else: ?>
-                <li><a href="/logout">Logout</a></li>
+                <li class="mx-2"><a href="/logout"><i class="fa-solid fa-power-off"></i><span class="hidden">Logout</span></a></li>
             <?php endif; ?>
         </ul>
     </nav>
-    <h1 class="text-5xl flex py-12 justify-center text-center">RUNNING UP</br>THAT HILL</h1>
     <?php if (!empty($_SESSION['user'])): ?>
         <span>Hello <?= $_SESSION['user']['username'] ?></span>
     <?php endif; ?>
