e96197743cc3f196b173f599573f0060b90e1693
l4ur4nn3 <nicolaslauranne99@gmail.com>
2022-12-21 12:07:59 +0100

    temp

--- src/public/index.php
+++ src/public/index.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 session_start();
 
 require 'vendor/autoload.php';
+require_once 'config.php';
 
 $url = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 $method = $_SERVER['REQUEST_METHOD'];
@@ -55,17 +56,3 @@ if ($url === 'registration') {
     }
 }
 ?>
-
-
-<head>
-    <!-- <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title -->
-    <link href="./index.css" rel="stylesheet">
-    <!-- <style>
-        body {
-            background-color: rgb(217 119 6);
-        }
-    </style> -->
-</head>
